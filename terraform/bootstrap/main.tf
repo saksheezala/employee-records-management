5ad4f6c6b323aa16b613fe1d51e6694b4a340763
Sakshee Zala <saksheezala1111.com>
2026-08-02 01:44:05 +0530

    Bootstrap: grant DevOps SP Storage Account Contributor for Terraform state access

--- terraform/bootstrap/main.tf
+++ terraform/bootstrap/main.tf
@@ -25,3 +25,12 @@ resource "azurerm_storage_container" "tfstate" {
   storage_account_name  = azurerm_storage_account.tfstate.name
   container_access_type = "private"
 }
+
+# Grant the Azure DevOps Service Principal permission to read/write
+# Terraform state in this storage account. Required for 'terraform init'
+# to connect to the azurerm backend from the CI/CD pipeline.
+resource "azurerm_role_assignment" "cicd_sp_tfstate_access" {
+  scope                = azurerm_storage_account.tfstate.id
+  role_definition_name = "Storage Account Contributor"
+  principal_id         = var.cicd_principal_object_id
+}
